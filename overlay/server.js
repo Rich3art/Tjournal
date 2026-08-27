c567e98a6cad4cdb5fbc97143508a87326db5e67
Rich3art <85309098+Rich3art@users.noreply.github.com>
2026-08-27 21:09:37 +0400

    Add Passenger server entrypoint

---
+++ overlay/server.js
@@ -0,0 +1,14 @@
+const { createServer } = require("http");
+const next = require("next");
+
+const dev = process.env.NODE_ENV !== "production";
+const hostname = process.env.HOSTNAME || "127.0.0.1";
+const port = parseInt(process.env.PORT || "3000", 10);
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {
+  createServer((req, res) => handle(req, res)).listen(port, hostname, () => {
+    console.log(`Journedge ready on http://${hostname}:${port}`);
+  });
+});
